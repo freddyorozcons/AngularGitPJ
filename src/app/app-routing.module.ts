ae8569daaebcf68bfff4e15daaa8941017d6ebcb
freddyorozcons <freddy.orozco.newshore@gmail.com>
2019-06-03 15:06:14 -0500

    Fixed rebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent }
 ];
-=======
-
-const routes: Routes = [];
->>>>>>> initial commit
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-<<<<<<< HEAD
 
 export class AppRoutingModule { 
 
 }
 
 
-=======
-export class AppRoutingModule { }
->>>>>>> initial commit
